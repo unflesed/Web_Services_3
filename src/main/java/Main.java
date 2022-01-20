fadc9d87ebc2b95bd755e9ee584d0ec55c5a997c
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2022-01-20 21:53:20 +0300

    LR_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
 import java.io.File;
@@ -7,10 +8,26 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws JAXBException {
         Catalog catalog = new Catalog();
+        CD cd1 = new CD("title1", "artist1", "Russia", "company1", 100.5, 1999);
+        CD cd2 = new CD("title2", "artist2", "Belarus", "company2", 200.5, 2000);
+        CD cd3 = new CD("title3", "artist3", "Ukraine", "company3", 1200.5, 1989);
+
+        catalog.add(cd1);
+        catalog.add(cd2);
+        catalog.add(cd3);
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+
+        File file1 = new File("C:\\Users\\user\\IdeaProjects\\Web_Services_3\\src\\main\\resources\\output.xml");
+
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        marshaller.marshal(catalog, file1);
 
         File file = new File("C:\\Users\\user\\IdeaProjects\\Web_Services_3\\src\\main\\resources\\catalog.xml");
 
-        JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+
 
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
